e65814afbfb85630f0e1ebb184e5e9d83498af5e
Marcel Wilson <trenchrats@gmail.com>
2022-07-20 09:39:54 -0500

    Forgot to spec out the other mocked drivers

--- tests/test_abilities.py
+++ tests/test_abilities.py
@@ -18,26 +18,26 @@ class TestBrowseTheWeb:
         b = BrowseTheWeb(None)
         assert isinstance(b, Forgettable)
 
-    @mock.patch("screenpy_selenium.abilities.browse_the_web.Firefox")
+    @mock.patch("screenpy_selenium.abilities.browse_the_web.Firefox", spec=Firefox)
     def test_using_firefox(self, mocked_firefox):
         BrowseTheWeb.using_firefox()
 
         mocked_firefox.assert_called_once()
 
-    @mock.patch("screenpy_selenium.abilities.browse_the_web.Chrome")
+    @mock.patch("screenpy_selenium.abilities.browse_the_web.Chrome", spec=Chrome)
     def test_using_chrome(self, mocked_chrome):
         BrowseTheWeb.using_chrome()
 
         mocked_chrome.assert_called_once()
 
-    @mock.patch("screenpy_selenium.abilities.browse_the_web.Safari")
+    @mock.patch("screenpy_selenium.abilities.browse_the_web.Safari", spec=Safari)
     def test_using_safari(self, mocked_safari):
         BrowseTheWeb.using_safari()
 
         mocked_safari.assert_called_once()
 
     @mock.patch.dict(os.environ, {"IOS_DEVICE_VERSION": "1"})
-    @mock.patch("screenpy_selenium.abilities.browse_the_web.Remote")
+    @mock.patch("screenpy_selenium.abilities.browse_the_web.Remote", spec=Remote)
     def test_using_ios(self, mocked_remote):
         BrowseTheWeb.using_ios()
 
@@ -48,7 +48,7 @@ class TestBrowseTheWeb:
             BrowseTheWeb.using_ios()
 
     @mock.patch.dict(os.environ, {"ANDROID_DEVICE_VERSION": "1"})
-    @mock.patch("screenpy_selenium.abilities.browse_the_web.Remote")
+    @mock.patch("screenpy_selenium.abilities.browse_the_web.Remote", spec=Remote)
     def test_using_android(self, mocked_android):
         BrowseTheWeb.using_android()
 
