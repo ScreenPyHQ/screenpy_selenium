5063b6288abe2895e8434af5a2d53047c8f11461
Perry Goy <perry.goy@gmail.com>
2023-07-19 22:37:45 -0500

    #26: add present-tense aliases for all Actions.

--- screenpy_selenium/actions/__init__.py
+++ screenpy_selenium/actions/__init__.py
@@ -28,53 +28,118 @@ from .switch_to_tab import SwitchToTab
 from .wait import Wait
 
 # Natural-language-enabling syntactic sugar
-ContextClick = RightClick
-Hover = MoveMouse
-Press = Enter
+AcceptsAlert = AcceptAlert
+Chains = Chain
+Clears = Clear
+Clicks = Click
+ContextClick = ContextClicks = RightClick
+DismissesAlert = DismissAlert
+DismissTheAlert = DismissesTheAlert = DismissAlert
+DoubleClicks = DoubleClick
+Enters = Enter
+Enters2FAToken = Enter2FAToken
+GoesBack = GoBack
+GoesForward = GoForward
+HoldsDown = HoldDown
+Hover = Hovers = MoveMouse
+MovesMouse = MoveMouse
+Press = Presses = Enter
 Pauses = Pause
-Refresh = Reload = ReloadPage = RefreshPage
-RespondToPrompt = RespondToThePrompt
-SwitchToWindow = SwitchToTab
-TakeScreenshot = SaveScreenshot
-Visit = Open
+Refresh = Refreshes = RefreshPage
+Reload = Reloads = RefreshPage
+ReloadPage = ReloadsPage = RefreshPage
+RefreshesPage = RefreshPage
+Releases = Release
+RespondToPrompt = RespondsToPrompt = RespondToThePrompt
+RespondsToThePrompt = RespondToThePrompt
+RightClicks = RightClick
+SavesConsoleLog = SaveConsoleLog
+SavesScreenshot = SaveScreenshot
+Selects = Select
+SelectsByIndex = SelectByIndex
+SelectsByText = SelectByText
+SelectsByValue = SelectByValue
+SwitchesTo = SwitchTo
+SwitchesToTab = SwitchToTab
+SwitchToWindow = SwitchesToWindow = SwitchToTab
+TakeScreenshot = TakesScreenshot = SaveScreenshot
+Visit = Visits = Open
+Opens = Open
+Waits = Wait
 
 __all__ = [
     "AcceptAlert",
+    "AcceptsAlert",
     "Chain",
+    "Chains",
     "Clear",
+    "Clears",
     "Click",
+    "Clicks",
     "ContextClick",
+    "ContextClicks",
     "DismissAlert",
+    "DismissesAlert",
     "DoubleClick",
+    "DoubleClicks",
     "Enter",
     "Enter2FAToken",
+    "Enters",
+    "Enters2FAToken",
     "GoBack",
+    "GoesBack",
+    "GoesForward",
     "GoForward",
     "HoldDown",
+    "HoldsDown",
     "Hover",
+    "Hovers",
     "MoveMouse",
+    "MovesMouse",
     "Open",
+    "Opens",
     "Pause",
     "Pauses",
     "Press",
+    "Presses",
     "Refresh",
+    "Refreshes",
+    "RefreshesPage",
     "RefreshPage",
     "Release",
+    "Releases",
     "Reload",
     "ReloadPage",
+    "Reloads",
+    "ReloadsPage",
+    "RespondsToPrompt",
+    "RespondsToThePrompt",
     "RespondToPrompt",
     "RespondToThePrompt",
     "RightClick",
+    "RightClicks",
     "SaveConsoleLog",
+    "SavesConsoleLog",
     "SaveScreenshot",
+    "SavesScreenshot",
     "Select",
     "SelectByIndex",
     "SelectByText",
     "SelectByValue",
+    "Selects",
+    "SelectsByIndex",
+    "SelectsByText",
+    "SelectsByValue",
+    "SwitchesTo",
+    "SwitchesToTab",
+    "SwitchesToWindow",
     "SwitchTo",
     "SwitchToTab",
     "SwitchToWindow",
     "TakeScreenshot",
+    "TakesScreenshot",
     "Visit",
+    "Visits",
     "Wait",
+    "Waits",
 ]
