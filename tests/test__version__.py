6b3062f4e43ac3860bfa7eed99c861631b7224ca
Marcel Wilson <trenchrats@gmail.com>
2023-09-22 15:03:18 -0500

    fixing https://github.com/ScreenPyHQ/screenpy_selenium/issues/36

--- tests/test__version__.py
+++ tests/test__version__.py
@@ -2,6 +2,6 @@ from screenpy_selenium import __version__
 
 
 def test_metadata() -> None:
-    assert __version__.__title__ == "screenpy-selenium"
+    assert __version__.__title__ == "screenpy_selenium"
     assert __version__.__license__ == "MIT"
     assert __version__.__author__ == "Perry Goy"
