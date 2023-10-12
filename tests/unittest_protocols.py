507227e0f545f1725bceb6f3e77eeb875bcfcb63
Marcel Wilson <trenchrats@gmail.com>
2023-10-12 09:17:57 -0500

    fixing #38 - runtime protocol checks

--- tests/unittest_protocols.py
+++ tests/unittest_protocols.py
@@ -1,30 +1,30 @@
-from typing import Protocol
+from typing import Protocol, runtime_checkable
 
-from screenpy import Answerable, ErrorKeeper, Forgettable, Performable
+from screenpy import Answerable, Describable, ErrorKeeper, Forgettable, Performable
 
 from screenpy_selenium import Chainable
 
 
-class Describable(Protocol):
-    def describe(self) -> str:
-        ...
-
-
+@runtime_checkable
 class Question(Answerable, Describable, Protocol):
     ...
 
 
+@runtime_checkable
 class ErrorQuestion(Answerable, Describable, ErrorKeeper, Protocol):
     ...
 
 
+@runtime_checkable
 class Action(Performable, Describable, Protocol):
     ...
 
 
+@runtime_checkable
 class ChainableAction(Chainable, Performable, Describable, Protocol):
     ...
 
 
+@runtime_checkable
 class Ability(Forgettable, Protocol):
     ...
